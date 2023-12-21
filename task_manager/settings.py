c6172a8e764fcf9c35c6d13cf0979cc420a55903
deusmg <laptevap@gmail.com>
2023-12-21 21:29:44 +0300

    add django-bootstrap5, i18n

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -42,6 +42,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_bootstrap5',
+    'task_manager',
+    'whitenoise.runserver_nostatic',
 ]
 
 MIDDLEWARE = [
@@ -52,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'task_manager.urls'
@@ -122,6 +126,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
--- task_manager/views.py
+++ task_manager/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views import View
+from django.utils.translation import gettext
 
-def home(request):
-    return HttpResponse("Добрый день!")
+
+class IndexView(View):
+    def get(self, request, *args, **kwargs):
+        hello_from_hexlet = gettext("Hello from Hexlet!")
+        coding_courses = gettext('Practical programming courses')
+        read_more = gettext("Read more")
+        exit = gettext("Exit")
+        return render(request, 'index.html',
+                    context={'hello_from_hexlet': hello_from_hexlet,
+                            'coding_courses': coding_courses,
+                            'read_more': read_more,
+                            'exit': exit,
+                            })
