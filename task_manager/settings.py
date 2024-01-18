7ef72b663bc2f73985a58116868092652ef3d77c
deusmg <laptevap@gmail.com>
2024-01-19 01:30:03 +0300

    fix linter

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -67,7 +67,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    #'rollbar.contrib.django.middleware.RollbarNotifierMiddleware',
 ]
 
 ROOT_URLCONF = 'task_manager.urls'
