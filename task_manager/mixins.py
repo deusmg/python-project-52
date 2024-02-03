0e67a1ed457e93e87397a6661333d918e8336d74
deusmg <laptevap@gmail.com>
2024-02-03 17:45:36 +0300

    edit mixins.py

--- task_manager/mixins.py
+++ task_manager/mixins.py
@@ -2,6 +2,7 @@ from django.contrib import messages
 from django.contrib.auth.mixins import UserPassesTestMixin, LoginRequiredMixin
 from django.db.models import ProtectedError
 from django.shortcuts import redirect
+from django.utils.translation import gettext as _
 
 
 class UserPermissionMixin(UserPassesTestMixin):
@@ -17,16 +18,27 @@ class UserPermissionMixin(UserPassesTestMixin):
         return redirect(self.permission_url)
 
 
-class UserAuthRequiredMixin(LoginRequiredMixin):
+class PermissionDeniedMixin:
+
+    permission_denied_message = None
+    permission_denied_url = None
+
+    def handle_no_permission(self):
+        messages.error(self.request, self.permission_denied_message)
+        return redirect(self.permission_denied_url)
+
+
+class UserAuthRequiredMixin(LoginRequiredMixin, PermissionDeniedMixin):
 
     def dispatch(self, request, *args, **kwargs):
         if not self.request.user.is_authenticated:
-            messages.error(self.request, self.permission_denied_message)
-            return redirect(self.login_url)
+            self.permission_denied_message = _('You must to be log in')
+            self.permission_denied_url = self.login_url
+            return self.handle_no_permission()
         return super().dispatch(request, *args, **kwargs)
 
 
-class ObjectDeleteProtectionMixin:
+class ObjectDeleteProtectionMixin(PermissionDeniedMixin):
 
     protection_message = None
     protected_url = None
@@ -35,17 +47,19 @@ class ObjectDeleteProtectionMixin:
         try:
             return super().post(request, *args, **kwargs)
         except ProtectedError:
-            messages.error(request, self.protection_message)
-            return redirect(self.protected_url)
+            self.permission_denied_message = self.protection_message
+            self.permission_denied_url = self.protected_url
+            return self.handle_no_permission()
 
 
-class TaskAuthorPermissionMixin:
+class TaskAuthorPermissionMixin(UserPassesTestMixin, PermissionDeniedMixin):
 
     author_url = None
     author_message = None
 
-    def dispatch(self, request, *args, **kwargs):
-        if request.user.id != self.get_object().author.id:
-            messages.error(request, self.author_message)
-            return redirect(self.author_url)
-        return super().dispatch(request, *args, **kwargs)
+    def test_func(self):
+        return self.request.user.id == self.get_object().author.id
+
+    def handle_no_permission(self):
+        messages.error(self.request, self.author_message)
+        return redirect(self.author_url)
